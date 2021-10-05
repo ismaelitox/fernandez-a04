9f58f937fcea2700ec378d3e5df1e210c43d83fa
ismaelitox <84604631+ismaelitox@users.noreply.github.com>
2021-10-05 00:08:35 -0400

    learning from mistakes

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,37 @@
+
+/*
+ * UCF COP3330 Fall 2021 Assignment 4 Solutions
+ * Copyright 2021 Jonah Fernandez
+ */
+
+
+package baseline;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.util.ArrayList;
+
+public class Solution41 {
+
+    public static void main(String[] args) {
+
+
+        // Program will read form list of names from a text file named 'exercise41_input.txt'
+        // Program will have a method or methods to sort alphabetically
+        // Program will print list of names alphabetically to a text file named 'exercise41_output.txt'
+        // Output text will display number of names along with a marker then a column of names sorted.
+    }
+    // Will most likely use Reader object to read in file
+    private static ArrayList<String> fileReader(BufferedReader inputFile, String file){
+        return String ;
+    }
+    // Will create a listArray object to hold lines of the input text
+    // will sort the input lines using  separate method from within java
+    // will use a Writer object to write into the new file
+    private static BufferedWriter fileWriter(BufferedWriter outputFile, ArrayList<String>, sorted ){
+
+    }
+    // going to have to figure out to add to the file as well
+
+
+}
