05af8a315a764fb6337240e1c7458c36a7bca8cd
ismaelitox <84604631+ismaelitox@users.noreply.github.com>
2021-10-17 23:53:25 -0400

    sorry for the waste of time, struggling in my other classes

--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -7,16 +7,24 @@
 
 package baseline;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Solution41 {
 
+    private static BufferedWriter writeFile;
+    static BufferedReader readFile;
+
     public static void main(String[] args) {
 
+
+        // Data from text file will be stored in a String Array with variable 'Text Lines'
+        ArrayList<String> textLines = fileReader(readFile);
+        // This is the actual method used to sort the textLines
+        Collections.sort(textLines);
+        // String data from fileWriter method will be written to ex41_output.txt
+        writeFile = fileWriter(writeFile, textLines);
 
         // Program will read form list of names from a text file named 'exercise41_input.txt'
         // Program will have a method or methods to sort alphabetically
@@ -24,28 +32,29 @@ public class Solution41 {
         // Output text will display number of names along with a marker then a column of names sorted.
     }
     // Will most likely use Reader object to read in file
-    private static ArrayList<String> fileReader(BufferedReader inFile, String file){
+    private static ArrayList<String> fileReader(BufferedReader inFile){
         // Using array list to do this.
         ArrayList<String> inputText = new ArrayList<>();
 
         // try block for error handling
         try{
             // Buffered reader object creating a new file reader object
-            inFile = new BufferedReader(new FileReader(file));
-
+            inFile = new BufferedReader(new FileReader("data/exercise41_input.txt"));
+            // helper that iterates along line
             var textReader = inFile.readLine();
-
+            // while end hasn't been reached
             while (textReader != null){
-
+                // add to
                 inputText.add(textReader);
                 textReader = inFile.readLine();
             }
+            // error handling
         }catch (IOException exception){
             exception.printStackTrace();
         }finally {
 
             try{
-
+                // error handle for null
                 if(inFile != null){
                     inFile.close();
                 }
@@ -59,10 +68,37 @@ public class Solution41 {
     // Will create a listArray object to hold lines of the input text
     // will sort the input lines using  separate method from within java
     // will use a Writer object to write into the new file
-   // private static BufferedWriter fileWriter(BufferedWriter outputFile, ArrayList<String>, sorted ){
-
-   // }
     // going to have to figure out to add to the file as well
 
+    // Method that will use BW to write sorted + formatted text to output file
+    private static BufferedWriter fileWriter(BufferedWriter outputFile, ArrayList<String> sortedText) {
+        // Try block to prevent crash if file doesn't write
+        try {
+            // Want to output file to be written to this destination
+            outputFile = new BufferedWriter(new FileWriter("data/exercise41_output.txt"));
+            // int will return number of lines without hardcoding actual number
+            int numLines = sortedText.size();
+            // Formatted output to go to top of text File
+            outputFile.write("Total of " + numLines + " names\n------------\n");
+
+            for (String lineMarker : sortedText) {
+                outputFile.write(lineMarker);
+                outputFile.newLine();
+            }
+        return outputFile;
+            // File Not found
+        } catch //
+         (IOException exception){ exception.printStackTrace();
+        } finally {
+            try {
+                // EOF, Close the file
+                if (outputFile != null) outputFile.close();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+        // returns file for testing
+        return outputFile;
+    }
 
 }
